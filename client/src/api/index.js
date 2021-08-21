a6e21e7253bee7528d391cab266f9fdedb3ec721
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-08-21 16:46:26 -0400

    make it better

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 // adapted from: https://www.youtube.com/watch?v=ngc9gnGgUdA&list=PL6QREj8te1P7VSwhrMf3D3Xt4V6_SRkhu&index=1
 // adapted from: https://www.youtube.com/watch?v=aibtHnbeuio&list=PL6QREj8te1P7VSwhrMf3D3Xt4V6_SRkhu&index=2
 import axios from "axios";
-
 const url = "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(url);
