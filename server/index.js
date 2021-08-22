62f2d61433ae24d14d08228a8af4469daf66294e
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-08-22 00:19:38 -0400

    make it better

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 //connecting to database and running it (Mongodb)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
